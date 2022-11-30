9021f7feea257bb171ea00ac57e24dc6ed324795
s473618 <zhukovskaja.elena@gmail.com>
2022-11-30 14:06:48 +0100

    comment1

--- c3p2a.c
+++ c3p2a.c
@@ -11,21 +11,6 @@
 
 #define PORT htons(8080)
 
-/*
-  int main(void) {
-  DIR *d;
-  struct dirent *dir;
-  d = opendir(".");
-  if (d) {
-    while ((dir = readdir(d)) != NULL) {
-      printf("%s\n", dir->d_name);
-    }
-    closedir(d);
-  }
-  return(0);
-}
-*/
-
 char showTheList(char* path){
     Dir* d;
     struct dirent* dir;
